bd2f05cffa1ac5f6d3e642c20811d1f3e4791022
Empathies <mathiaspeene@gmail.com>
2021-01-11 13:48:07 +0100

    Prize form wired up

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -6,11 +6,23 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
+            if (database)
+            {
+                // TODO - Set up the SQL Connection properly
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
 
+            if (textFiles)
+            {
+                // TODO - Set up the Text Connection properly
+                TextConnection text = new TextConnection();
+                Connections.Add(text);
+            }
         }
     }
 }
--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -7,6 +7,11 @@ namespace TrackerLibrary
     public class PrizeModel
     {
         /// <summary>
+        /// The unique identifier for the prize
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
         /// Defines the PlaceNumber that wins this prize
         /// </summary>
         public int PlaceNumber { get; set; }
@@ -25,5 +30,27 @@ namespace TrackerLibrary
         /// Defines the amount of prizemoney in a percentage
         /// </summary>
         public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+            
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+        }
     }
 }
