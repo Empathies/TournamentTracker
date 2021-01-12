6b9be0ab47ce8892ed14787d3eec0b67970c49c4
Empathies <mathiaspeene@gmail.com>
2021-01-12 14:01:29 +0100

    Create Person Text implemented

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -10,10 +10,36 @@ namespace TrackerLibrary.DataAccess
     public class TextConnector : IDataConnection
     {
         private const string PrizesFile = "PrizeModels.csv";
+        private const string PeopleFile = "PersonModels.csv";
 
+        /// <summary>
+        /// Saves a new person to the database
+        /// </summary>
+        /// <param name="model">The person information.</param>
+        /// <returns>The person information, including the unique identifier.</returns>
         public PersonModel CreatePerson(PersonModel model)
         {
-            throw new NotImplementedException();
+            // Load the text file
+            // Convert the text to List<PersonModel>
+            List<PersonModel> people = PeopleFile.FullFilePath().LoadFile().ConvertToPersonModels();
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (people.Count > 0)
+            {
+                currentId = people.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max +1)
+            people.Add(model);
+
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+            people.SaveToPeopleFile(PeopleFile);
+
+            return model;
         }
 
         /// <summary>
@@ -23,8 +49,8 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            // **Load the text file
-            // **Convert the text to List<PrizeModel>
+            // Load the text file
+            // Convert the text to List<PrizeModel>
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
             // Find the max ID
