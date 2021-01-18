f2b084238174bbdf2e98bb167e3423cf18fd503a
Empathies <mathiaspeene@gmail.com>
2021-01-18 12:33:46 +0100

    Save Tournament rounds to SQL implemented

--- TrackerLibrary/Models/MatchupEntryModel.cs
+++ TrackerLibrary/Models/MatchupEntryModel.cs
@@ -5,6 +5,10 @@ namespace TrackerLibrary.Models
     public class MatchupEntryModel
     {
         /// <summary>
+        /// The unique identifier for the matchup entry
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// Represents one Team in the matchup.
         /// </summary>
         public TeamModel TeamCompeting { get; set; }
